6298b0e660a16f6bf11345a1ee91b414098694f1
Rafiul Islam <rafi.cse.ru@gmail.com>
2017-07-04 00:59:12 +0600

    After working in the Start and End button.

--- app/src/main/java/com/aktar/sakifa/locationalarm/EndTask.java
+++ app/src/main/java/com/aktar/sakifa/locationalarm/EndTask.java
@@ -2,12 +2,18 @@ package com.aktar.sakifa.locationalarm;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class EndTask extends Activity {
+
+    ImageView end_task_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_task);
+
+        end_task_button = (ImageView) findViewById(R.id.EndTaskButton);
+
     }
 }
--- app/src/main/java/com/aktar/sakifa/locationalarm/StartTask.java
+++ app/src/main/java/com/aktar/sakifa/locationalarm/StartTask.java
@@ -2,14 +2,18 @@ package com.aktar.sakifa.locationalarm;
 
 import android.app.Activity;
 import android.os.Bundle;
-
-
+import android.widget.ImageView;
 
 public class StartTask extends Activity {
+
+    ImageView start_task_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_task);
+
+        start_task_button = (ImageView) findViewById(R.id.StartTaskButton);
+
     }
 }
